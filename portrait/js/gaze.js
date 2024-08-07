a42f3f43ba33f9bdb611eb114027dbd476b54948
veelatseng <veela.tzeng@gmail.com>
2024-08-07 10:29:22 +0800

    更新前台畫面

---
+++ portrait/js/gaze.js
@@ -0,0 +1,55 @@
+
+function formEye(obj) {
+  const documentHtml = document.querySelector('html');
+  const webLang = documentHtml.getAttribute('lang');
+  // form password eyes
+  let checkEye = document.querySelectorAll('.passwordEye');
+  checkEye.forEach((item) => {
+    let passwordInput = item.parentNode.querySelector('[type="password"]');
+    item.addEventListener('click', function (e) {
+      if (e.target.classList.contains('hide')) {
+        //換class / type
+        e.target.classList.remove('hide');
+        e.target.classList.add('show');
+        if (webLang) {
+          obj.password.data.forEach((s) => {
+            if (webLang.slice(0, 2) == s.lang) {
+              e.target.textContent = s.hide;
+            }
+          });
+        }
+        passwordInput.setAttribute('type', 'text');
+      } else {
+        passwordInput.setAttribute('type', 'password');
+        e.target.classList.remove('show');
+        e.target.classList.add('hide');
+        e.target.textContent = obj.show;
+        if (webLang) {
+          obj.password.data.forEach((s) => {
+            if (webLang.slice(0, 2) == s.lang) {
+              e.target.textContent = s.show;
+            }
+          });
+        }
+      }
+    });
+  });
+}
+
+formEye({
+  password: {
+    data: [
+      {
+        lang: 'zh',
+        show: '顯示密碼',
+        hide: '隱藏密碼',
+      },
+      //...由此新增其他語系
+    ],
+    //預設語系
+    default: {
+      show: 'show',
+      hide: 'hide',
+    },
+  },
+});
