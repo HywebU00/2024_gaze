e58d38570d77ea137c2f6f713054dee4fd9ae32c
veelatseng <veela.tzeng@gmail.com>
2024-08-14 15:29:40 +0800

    後台管理系統完成

---
+++ cms/js/chart.js
@@ -0,0 +1,264 @@
+
+// 設備借用週統計
+new Chart(document.getElementById("chartWeek"), {
+  type: "line",
+  data: {
+    labels: ["週一", "週二", "週三", "週四", "週五", "週六", "週日"],
+    datasets: [
+      {
+        data: [30, 45, 60, 15, 18, 20, 15 ],
+        label: "借閱數量(台）",
+        borderColor: "#ba6000",
+        backgroundColor: "#ba6000",
+        pointRadius: 4,
+        pointHoverRadius: 4,
+        fill: false,
+      },
+    ],
+  },
+  options: {
+    layout: {
+      padding: {
+        left: 40,
+        right: 40,
+        top: 40,
+        bottom: 40
+      }
+    },
+    elements: {
+      line: {
+        tension: 0,
+      },
+    },
+    label: {},
+    legend: {
+      display: true,
+      position: "bottom",
+      labels: {
+        // boxWidth: 50,
+        fontSize: 16,
+        fontColor: "#534741",
+        fontStyle: "normal",
+        padding: 16,
+      },
+    },
+    tooltips: {
+      mode: "point",
+      backgroundColor: "#534741",
+      fontColor: "#222222",
+      yPadding: 10,
+      xPadding: 10,
+      titleAlign: "center",
+      bodyAlign: "center",
+      displayColors: false,
+    },
+    scales: {
+      xAxes: [
+        {
+          ticks: {
+            fontSize: 14,
+            fontColor: "#534741",
+          },
+          gridLines: {
+            color: "#a3948b",
+            zeroLineColor: "#a3948b",
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            fontSize: 14,
+            fontColor: "#534741",
+          },
+          gridLines: {
+            color: "#a3948b",
+            zeroLineColor: "#a3948b",
+          },
+        },
+      ],
+    },
+  },
+});
+
+
+// new Chart(document.getElementById("chartUser1"), {
+//   type: "pie",
+//   data: {
+//     datasets: [
+//       {
+//         data: [39486, 12088],
+//         backgroundColor: [
+//           "#F765A3",
+//           "#A155B9",
+//         ],
+//         borderWidth: [0, 0, 0, 0, 0],
+//         hoverBackgroundColor: [
+//           "#F765A3",
+//           "#A155B9",
+//         ],
+//       },
+//     ],
+//     labels: [
+//       "偽冒公眾人物",
+//       "偽冒政府機構",
+//     ],
+//   },
+//   options: {
+//     animation: {
+//       animateRotate: true,
+//       animateScale: true,
+//     },
+//     legend: {
+//       display: true,
+//       position: "bottom",
+//       labels: {
+//         fontColor: "#534741",
+//         fontSize: 16,
+//         padding: 10,
+//       },
+//     },
+//     tooltips: {
+//       // enabled: true,
+//       displayColors: false,
+//     },
+//     responsive: true,
+
+//   },
+// });
+
+var ctx = document.getElementById('chartUser1').getContext('2d');
+
+var myChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    datasets: [{
+      data: [11.2, 12.8],
+      backgroundColor: ['#ffaf00', '#ba6000'],
+      borderWidth: [0, 0],
+      hoverBackgroundColor: ['#ffaf00', '#ba6000'],
+      hoverBorderColor: ['#ffaf00', '#ba6000'],
+      hoverBorderWidth: [0, 0, 0]
+    }],
+    labels: [
+      '女性', '男性'
+    ]
+  },
+  options: {
+    layout: {
+      padding: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
+      }
+    },
+    animation: {
+      animateRotate: true,
+      animateScale: true
+    },
+    legend: {
+      display: true,
+      position: "bottom",
+      labels: {
+        fontSize: 16,
+        fontColor: "#534741",
+        fontStyle: "normal",
+        padding: 16,
+      },
+    },
+    tooltips: {
+      enabled: false
+    },
+    plugins: {
+      labels: {
+        fontSize: 16,
+        precision: 1,
+        fontColor: ['black', 'black',],
+        position: 'border'
+      }
+    }
+  }
+})
+
+// 設備借用週統計
+new Chart(document.getElementById("chartUser2"), {
+  type: "line",
+  data: {
+    labels: ["0歲", "10歲", "20歲", "30歲", "40歲", "50歲", "60歲", "70歲", "80歲", "90歲"],
+    datasets: [
+      {
+        data: [30, 45, 60, 15, 18, 20, 15, 18, 20, 15 ],
+        label: "借閱數量(台）",
+        borderColor: "#ba6000",
+        backgroundColor: "#ba6000",
+        pointRadius: 4,
+        pointHoverRadius: 4,
+        fill: false,
+      },
+    ],
+  },
+  options: {
+    layout: {
+      padding: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
+      }
+    },
+    elements: {
+      line: {
+        tension: 0,
+      },
+    },
+    label: {},
+    legend: {
+      display: true,
+      position: "bottom",
+      labels: {
+        // boxWidth: 50,
+        fontSize: 16,
+        fontColor: "#534741",
+        fontStyle: "normal",
+        padding: 16,
+      },
+    },
+    tooltips: {
+      mode: "point",
+      backgroundColor: "#534741",
+      fontColor: "#222222",
+      yPadding: 10,
+      xPadding: 10,
+      titleAlign: "center",
+      bodyAlign: "center",
+      displayColors: false,
+    },
+    scales: {
+      xAxes: [
+        {
+          ticks: {
+            fontSize: 14,
+            fontColor: "#534741",
+          },
+          gridLines: {
+            color: "#a3948b",
+            zeroLineColor: "#a3948b",
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            fontSize: 14,
+            fontColor: "#534741",
+          },
+          gridLines: {
+            color: "#a3948b",
+            zeroLineColor: "#a3948b",
+          },
+        },
+      ],
+    },
+  },
+});
